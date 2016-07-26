812ec91af378bd5d673e570a4d3718748540c9ec
CS 4220 <cs4220@dugs-imac.local>
2016-07-26 09:30:26 -0500

    Initial Commit

---
+++ ThreewittDugP4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreewittDugP4
+//
+//  Created by CS 4220 on 7/26/16.
+//  Copyright © 2016 CS 4220. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ThreewittDugP4/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ThreewittDugP4
+//
+//  Created by CS 4220 on 7/26/16.
+//  Copyright © 2016 CS 4220. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
