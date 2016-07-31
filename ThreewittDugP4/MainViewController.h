430f058d5f9d8dec3f323e2498d5bb4bcabd5762
Dug Threewitt <DugThreewitt@gfxintelmac.local>
2016-07-31 10:09:57 -0500

    Version .13 starting over

---
+++ ThreewittDugP4/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  ThreewittDugP4
+//
+//  Created by Dug Threewitt on 7/31/16.
+//  Copyright © 2016 CS 4220. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
